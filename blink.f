dcc5d4561699076ae66dd409ffeafdc8c0ec551c
Phil Howlett <phil.g.howlett@gmail.com>
2021-08-15 23:37:16 +1000

    blink now uses the HAL

--- blink.f
+++ blink.f
@@ -4,33 +4,29 @@
 \ ***************************************************************************
 \ Simpy blinks the LED on PORTB bit5 (Pin 19)
 
+\ Requires hal.f
+
 -blink
 marker -blink
 
 \ Setup some constants for the ATMega pins for us to use
-$0023 constant PINB
-$0024 constant DDRB
-$0025 constant PORTB
-
-%00100000 constant PB5
 
 : init ( -- )
-    PB5 DDRB mset ; \ Make PB5 as an output
+    PIN5 DDRB mset ; \ Make PB5 as an output
 
-: on ( pin -- )
-    PORTB mset ;
+: on ( -- )
+    PIN5 PORTB mset ;
 
-: off ( pin -- )
-    PORTB mclr ;
+: off ( -- )
+    PIN5 PORTB mclr ;
 
 : toggle ( pin -- )
-    PINB mset ;
+    PIN5 PINB mset ;
 
 \ Main loop
 : blink ( pin -- )
     init 
     begin
-        dup 
         toggle #500 ms
     again ;
 
